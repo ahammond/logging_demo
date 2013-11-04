537be8dab77b7c95a2e33139463b001a2d893a3e
Andrew Hammond <andrew.george.hammond@gmail.com>
2013-11-04 14:35:59 -0800

    using introspection for getLogger call

---
+++ introspective_getLogger.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from inspect import stack as inspect_stack
+from logging import getLogger
+from logging.config import fileConfig
+
+
+class A(object):
+    def __init__(self):
+        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l.debug('A instantiated')
+        l.info('A info')
+        l.error('A exploding!!!')
+
+    def my_method(self):
+        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l.debug('debug output in my_method')
+        l.info('info input in my_method')
+
+    def my_interesting_method(self):
+        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l.debug('debug output in my_interesting_method')
+        l.info('info input in my_interesting_method')
+
+    def my_noisy_method(self):
+        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        for x in range(1, 10):
+            l.info('noisy %d', x)
+        l.error('a real warning message!!!')
+
+
+class B(A):
+    def __init__(self):
+        super(B, self).__init__()
+        l = getLogger('{}.{}'.format(self.__class__.__name__, inspect_stack()[0][3]))
+        l.debug('B instantiated')
+        l.info('B info')
+        l.error('B exploding!!!')
+
+
+fileConfig('introspective_logging_config.ini')
+
+c = B()
+c.my_method()
+c.my_interesting_method()
+c.my_noisy_method()
