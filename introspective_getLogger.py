3973ff80b53c8efc2ae0b007b1a01008913ee2c4
Andrew Hammond <andrew.george.hammond@gmail.com>
2013-11-20 14:21:16 -0800

    go with python2 for simplicity

--- introspective_getLogger.py
+++ introspective_getLogger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 from inspect import stack as inspect_stack
 from logging import getLogger
@@ -7,23 +7,23 @@ from logging.config import fileConfig
 
 class A(object):
     def __init__(self):
-        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger("{0}.{1}".format(self.__class__.__name__, inspect_stack()[0][3]))
         l.debug('A instantiated')
         l.info('A info')
         l.error('A exploding!!!')
 
     def my_method(self):
-        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger("{0}.{1}".format(self.__class__.__name__, inspect_stack()[0][3]))
         l.debug('debug output in my_method')
         l.info('info input in my_method')
 
     def my_interesting_method(self):
-        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger("{0}.{1}".format(self.__class__.__name__, inspect_stack()[0][3]))
         l.debug('debug output in my_interesting_method')
         l.info('info input in my_interesting_method')
 
     def my_noisy_method(self):
-        l = getLogger("{}.{}".format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger("{0}.{1}".format(self.__class__.__name__, inspect_stack()[0][3]))
         for x in range(1, 10):
             l.info('noisy %d', x)
         l.error('a real warning message!!!')
@@ -32,14 +32,14 @@ class A(object):
 class B(A):
     def __init__(self):
         super(B, self).__init__()
-        l = getLogger('{}.{}'.format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger('{0}.{1}'.format(self.__class__.__name__, inspect_stack()[0][3]))
         l.debug('B instantiated')
         l.info('B info')
         l.error('B exploding!!!')
 
     def my_interesting_method(self):
         super(B, self).my_interesting_method()
-        l = getLogger('{}.{}'.format(self.__class__.__name__, inspect_stack()[0][3]))
+        l = getLogger('{0}.{1}'.format(self.__class__.__name__, inspect_stack()[0][3]))
         l.debug('debug output in my_interesting_method')
         l.info('info input in my_interesting_method')
 
